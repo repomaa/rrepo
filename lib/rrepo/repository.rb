9a6f2beed4f3ebc257be7f6fcfedd133c9ff2240
Joakim Reinert <mail@jreinert.com>
2015-03-30 15:31:17 +0200

    repository#all is now lazy

--- lib/rrepo/repository.rb
+++ lib/rrepo/repository.rb
@@ -52,7 +52,13 @@ module RRepo
 
     def all
       result = adapter.all(collection)
-      result.map(&method(:model_for))
+      Enumerator.new do |y|
+        loop do
+          doc = result.next_document
+          break if doc.nil
+          y << model_for(doc)
+        end
+      end
     end
 
     def find(id = nil, &block)
